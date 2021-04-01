ea9795ff1389f0f60e3a267552c194c7fbe8fd73
Victor Augusto <victor.augusto60@gmail.com>
2021-04-01 16:35:00 -0300

    Organizado pasta de meses

--- alelo/base/views.py
+++ alelo/base/views.py
@@ -25,12 +25,12 @@ def janeiro(request):
             return HttpResponseRedirect(reverse('base:janeiro'))
         else:
             valores_mes = Janeiro.objects.all()
-            return render(request, 'base/janeiro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/janeiro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_janeiro_dct = Janeiro.objects.aggregate(Sum('valor'))
     total_mes_janeiro = total_mes_janeiro_dct['valor__sum']
     valores_mes = Janeiro.objects.all()
-    return render(request, 'base/janeiro.html', {'valores_mes': valores_mes, 'total_mes_janeiro': total_mes_janeiro})
+    return render(request, 'base/meses/janeiro.html', {'valores_mes': valores_mes, 'total_mes_janeiro': total_mes_janeiro})
 
 
 def apagar_valor_janeiro(request, pk):
@@ -39,7 +39,7 @@ def apagar_valor_janeiro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:janeiro'))
-    return render(request, 'base/janeiro.html', {'valor': valor})
+    return render(request, 'base/meses/janeiro.html', {'valor': valor})
 
 
 # Mês - Fevereiro
@@ -52,12 +52,12 @@ def fevereiro(request):
             return HttpResponseRedirect(reverse('base:fevereiro'))
         else:
             valores_mes = Fevereiro.objects.all()
-            return render(request, 'base/fevereiro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/fevereiro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_fevereiro_dct = Fevereiro.objects.aggregate(Sum('valor'))
     total_mes_fevereiro = total_mes_fevereiro_dct['valor__sum']
     valores_mes = Fevereiro.objects.all()
-    return render(request, 'base/fevereiro.html',
+    return render(request, 'base/meses/fevereiro.html',
                   {'valores_mes': valores_mes, 'total_mes_fevereiro': total_mes_fevereiro})
 
 
@@ -67,7 +67,7 @@ def apagar_valor_fevereiro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:fevereiro'))
-    return render(request, 'base/fevereiro.html', {'valor': valor})
+    return render(request, 'base/meses/fevereiro.html', {'valor': valor})
 
 
 # Mês - Março
@@ -80,12 +80,12 @@ def marco(request):
             return HttpResponseRedirect(reverse('base:marco'))
         else:
             valores_mes = Marco.objects.all()
-            return render(request, 'base/marco.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/marco.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_marco_dct = Marco.objects.aggregate(Sum('valor'))
     total_mes_marco = total_mes_marco_dct['valor__sum']
     valores_mes = Marco.objects.all()
-    return render(request, 'base/marco.html', {'valores_mes': valores_mes, 'total_mes_marco': total_mes_marco})
+    return render(request, 'base/meses/marco.html', {'valores_mes': valores_mes, 'total_mes_marco': total_mes_marco})
 
 
 def apagar_valor_marco(request, pk):
@@ -94,7 +94,7 @@ def apagar_valor_marco(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:marco'))
-    return render(request, 'base/marco.html', {'valor': valor})
+    return render(request, 'base/meses/marco.html', {'valor': valor})
 
 
 # Mês - Abril
@@ -107,12 +107,12 @@ def abril(request):
             return HttpResponseRedirect(reverse('base:abril'))
         else:
             valores_mes = Abril.objects.all()
-            return render(request, 'base/abril.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/abril.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_abril_dct = Abril.objects.aggregate(Sum('valor'))
     total_mes_abril = total_mes_abril_dct['valor__sum']
     valores_mes = Abril.objects.all()
-    return render(request, 'base/abril.html', {'valores_mes': valores_mes, 'total_mes_abril': total_mes_abril})
+    return render(request, 'base/meses/abril.html', {'valores_mes': valores_mes, 'total_mes_abril': total_mes_abril})
 
 
 def apagar_valor_abril(request, pk):
@@ -121,7 +121,7 @@ def apagar_valor_abril(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:abril'))
-    return render(request, 'base/abril.html', {'valor': valor})
+    return render(request, 'base/meses/abril.html', {'valor': valor})
 
 
 # Mês - Maio
@@ -134,12 +134,12 @@ def maio(request):
             return HttpResponseRedirect(reverse('base:maio'))
         else:
             valores_mes = Maio.objects.all()
-            return render(request, 'base/maio.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/maio.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_maio_dct = Maio.objects.aggregate(Sum('valor'))
     total_mes_maio = total_mes_maio_dct['valor__sum']
     valores_mes = Maio.objects.all()
-    return render(request, 'base/maio.html', {'valores_mes': valores_mes, 'total_mes_maio': total_mes_maio})
+    return render(request, 'base/meses/maio.html', {'valores_mes': valores_mes, 'total_mes_maio': total_mes_maio})
 
 
 def apagar_valor_maio(request, pk):
@@ -148,7 +148,7 @@ def apagar_valor_maio(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:maio'))
-    return render(request, 'base/maio.html', {'valor': valor})
+    return render(request, 'base/meses/maio.html', {'valor': valor})
 
 
 # Mês - Junho
@@ -161,12 +161,12 @@ def junho(request):
             return HttpResponseRedirect(reverse('base:junho'))
         else:
             valores_mes = Junho.objects.all()
-            return render(request, 'base/junho.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/junho.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_junho_dct = Junho.objects.aggregate(Sum('valor'))
     total_mes_junho = total_mes_junho_dct['valor__sum']
     valores_mes = Junho.objects.all()
-    return render(request, 'base/junho.html', {'valores_mes': valores_mes, 'total_mes_junho': total_mes_junho})
+    return render(request, 'base/meses/junho.html', {'valores_mes': valores_mes, 'total_mes_junho': total_mes_junho})
 
 
 def apagar_valor_junho(request, pk):
@@ -175,7 +175,7 @@ def apagar_valor_junho(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:junho'))
-    return render(request, 'base/junho.html', {'valor': valor})
+    return render(request, 'base/meses/junho.html', {'valor': valor})
 
 
 # Mês - Julho
@@ -188,12 +188,12 @@ def julho(request):
             return HttpResponseRedirect(reverse('base:julho'))
         else:
             valores_mes = Julho.objects.all()
-            return render(request, 'base/julho.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/julho.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_julho_dct = Julho.objects.aggregate(Sum('valor'))
     total_mes_julho = total_mes_julho_dct['valor__sum']
     valores_mes = Julho.objects.all()
-    return render(request, 'base/julho.html', {'valores_mes': valores_mes, 'total_mes_julho': total_mes_julho})
+    return render(request, 'base/meses/julho.html', {'valores_mes': valores_mes, 'total_mes_julho': total_mes_julho})
 
 
 def apagar_valor_julho(request, pk):
@@ -202,7 +202,7 @@ def apagar_valor_julho(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:julho'))
-    return render(request, 'base/julho.html', {'valor': valor})
+    return render(request, 'base/meses/julho.html', {'valor': valor})
 
 
 # Mês - Agosto
@@ -215,12 +215,12 @@ def agosto(request):
             return HttpResponseRedirect(reverse('base:agosto'))
         else:
             valores_mes = Agosto.objects.all()
-            return render(request, 'base/agosto.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/agosto.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_agosto_dct = Agosto.objects.aggregate(Sum('valor'))
     total_mes_agosto = total_mes_agosto_dct['valor__sum']
     valores_mes = Agosto.objects.all()
-    return render(request, 'base/agosto.html', {'valores_mes': valores_mes, 'total_mes_agosto': total_mes_agosto})
+    return render(request, 'base/meses/agosto.html', {'valores_mes': valores_mes, 'total_mes_agosto': total_mes_agosto})
 
 
 def apagar_valor_agosto(request, pk):
@@ -229,7 +229,7 @@ def apagar_valor_agosto(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:agosto'))
-    return render(request, 'base/agosto.html', {'valor': valor})
+    return render(request, 'base/meses/agosto.html', {'valor': valor})
 
 
 # Mês - Setembro
@@ -242,12 +242,12 @@ def setembro(request):
             return HttpResponseRedirect(reverse('base:setembro'))
         else:
             valores_mes = Setembro.objects.all()
-            return render(request, 'base/setembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/setembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_setembro_dct = Setembro.objects.aggregate(Sum('valor'))
     total_mes_setembro = total_mes_setembro_dct['valor__sum']
     valores_mes = Setembro.objects.all()
-    return render(request, 'base/setembro.html', {'valores_mes': valores_mes, 'total_mes_setembro': total_mes_setembro})
+    return render(request, 'base/meses/setembro.html', {'valores_mes': valores_mes, 'total_mes_setembro': total_mes_setembro})
 
 
 def apagar_valor_setembro(request, pk):
@@ -256,7 +256,7 @@ def apagar_valor_setembro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:setembro'))
-    return render(request, 'base/setembro.html', {'valor': valor})
+    return render(request, 'base/meses/setembro.html', {'valor': valor})
 
 
 # Mês - Outubro
@@ -269,12 +269,12 @@ def outubro(request):
             return HttpResponseRedirect(reverse('base:outubro'))
         else:
             valores_mes = Outubro.objects.all()
-            return render(request, 'base/outubro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/outubro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_outubro_dct = Outubro.objects.aggregate(Sum('valor'))
     total_mes_outubro = total_mes_outubro_dct['valor__sum']
     valores_mes = Outubro.objects.all()
-    return render(request, 'base/outubro.html', {'valores_mes': valores_mes, 'total_mes_outubro': total_mes_outubro})
+    return render(request, 'base/meses/outubro.html', {'valores_mes': valores_mes, 'total_mes_outubro': total_mes_outubro})
 
 
 def apagar_valor_outubro(request, pk):
@@ -283,7 +283,7 @@ def apagar_valor_outubro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:outubro'))
-    return render(request, 'base/outubro.html', {'valor': valor})
+    return render(request, 'base/meses/outubro.html', {'valor': valor})
 
 
 # Mês - Novembro
@@ -296,12 +296,12 @@ def novembro(request):
             return HttpResponseRedirect(reverse('base:novembro'))
         else:
             valores_mes = Novembro.objects.all()
-            return render(request, 'base/novembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/novembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_novembro_dct = Novembro.objects.aggregate(Sum('valor'))
     total_mes_novembro = total_mes_novembro_dct['valor__sum']
     valores_mes = Novembro.objects.all()
-    return render(request, 'base/novembro.html', {'valores_mes': valores_mes, 'total_mes_novembro': total_mes_novembro})
+    return render(request, 'base/meses/novembro.html', {'valores_mes': valores_mes, 'total_mes_novembro': total_mes_novembro})
 
 
 def apagar_valor_novembro(request, pk):
@@ -310,7 +310,7 @@ def apagar_valor_novembro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:novembro'))
-    return render(request, 'base/novembro.html', {'valor': valor})
+    return render(request, 'base/meses/novembro.html', {'valor': valor})
 
 
 # Mês - Dezembro
@@ -323,12 +323,12 @@ def dezembro(request):
             return HttpResponseRedirect(reverse('base:dezembro'))
         else:
             valores_mes = Dezembro.objects.all()
-            return render(request, 'base/dezembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
+            return render(request, 'base/meses/dezembro.html', {'form': form, 'valores_mes': valores_mes}, status=400)
 
     total_mes_dezembro_dct = Dezembro.objects.aggregate(Sum('valor'))
     total_mes_dezembro = total_mes_dezembro_dct['valor__sum']
     valores_mes = Dezembro.objects.all()
-    return render(request, 'base/dezembro.html', {'valores_mes': valores_mes, 'total_mes_dezembro': total_mes_dezembro})
+    return render(request, 'base/meses/dezembro.html', {'valores_mes': valores_mes, 'total_mes_dezembro': total_mes_dezembro})
 
 
 def apagar_valor_dezembro(request, pk):
@@ -337,4 +337,4 @@ def apagar_valor_dezembro(request, pk):
     if request.method == 'POST':
         valor.delete()
         return HttpResponseRedirect(reverse('base:dezembro'))
-    return render(request, 'base/dezembro.html', {'valor': valor})
+    return render(request, 'base/meses/dezembro.html', {'valor': valor})
